af937f46143a7e2f4a2626002dd8b0efbb165717
Minh Nguyen <mnguyen0226@vt.edu>
2021-05-20 17:03:36 -0400

    start seq2seq

---
+++ rnn/text_translation/seq2seq/seq2seq_with_nn/utils/nn_model.py
@@ -0,0 +1,7 @@
+"""
+ABOUT: The model contains of 3 section: encoder, decoder, and seq2seq model that encapsulates the encoder and decoders and will provide a way to interface with each
+
+ENCODER:
+- contains 2 layers of LSTM
+
+"""
