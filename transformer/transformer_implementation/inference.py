f4c0cbc6a64cba51394d412950d3c6a0bae491bb
Minh Nguyen <mnguyen0226@vt.edu>
2021-05-27 13:47:44 -0400

    minor change

--- transformer/transformer_implementation/inference.py
+++ transformer/transformer_implementation/inference.py
@@ -1,5 +1,7 @@
 # Resource: https://github.com/bentrevett/pytorch-seq2seq/blob/master/6%20-%20Attention%20is%20All%20You%20Need.ipynb
 from main import *
+from utils.preprocess import *
+from utils.model import *
 
 """
 Inference
@@ -144,6 +146,7 @@ def run_test():
     test_training_set()
     test_validating_set()
     test_testing_set()
+    
 
 
 
