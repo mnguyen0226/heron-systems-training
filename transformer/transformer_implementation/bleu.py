36cd24cc18468690397620f60f17db5c577c79bf
Minh Nguyen <mnguyen0226@vt.edu>
2021-05-27 13:42:51 -0400

    Add inference and bleu metrics

---
+++ transformer/transformer_implementation/bleu.py
@@ -0,0 +1,99 @@
+from torchtext.data.metrics import bleu_score
+from main import *
+from inference import *
+
+def calculate_bleu(data, src_field, trg_field, model, device, max_len = 50):
+    
+    trgs = []
+    pred_trgs = []
+    
+    for datum in data:
+        
+        src = vars(datum)['src']
+        trg = vars(datum)['trg']
+        
+        pred_trg, _ = translate_sentence(src, src_field, trg_field, model, device, max_len)
+        
+        #cut off <eos> token
+        pred_trg = pred_trg[:-1]
+        
+        pred_trgs.append(pred_trg)
+        trgs.append([trg])
+        
+    return bleu_score(pred_trgs, trgs)
+
+def run_test():
+    bleu_score = calculate_bleu(test_data, SRC, TRG, model, device)
+
+    print(f'BLEU score = {bleu_score*100:.2f}')
+
+########################################################################
+# the calculate_bleu function is unoptimized, this is a faster version
+def translate_sentence_vectorized(src_tensor, src_field, trg_field, model, device, max_len=50):
+    assert isinstance(src_tensor, torch.Tensor)
+
+    model.eval()
+    src_mask = model.make_src_mask(src_tensor)
+
+    with torch.no_grad():
+        enc_src = model.encoder(src_tensor, src_mask)
+    # enc_src = [batch_sz, src_len, hid_dim]
+
+    trg_indexes = [[trg_field.vocab.stoi[trg_field.init_token]] for _ in range(len(src_tensor))]
+    # Even though some examples might have been completed by producing a <eos> token
+    # we still need to feed them through the model because other are not yet finished
+    # and all examples act as a batch. Once every single sentence prediction encounters
+    # <eos> token, then we can stop predicting.
+    translations_done = [0] * len(src_tensor)
+    for i in range(max_len):
+        trg_tensor = torch.LongTensor(trg_indexes).to(device)
+        trg_mask = model.make_trg_mask(trg_tensor)
+        with torch.no_grad():
+            output, attention = model.decoder(trg_tensor, enc_src, trg_mask, src_mask)
+        pred_tokens = output.argmax(2)[:,-1]
+        for i, pred_token_i in enumerate(pred_tokens):
+            trg_indexes[i].append(pred_token_i)
+            if pred_token_i == trg_field.vocab.stoi[trg_field.eos_token]:
+                translations_done[i] = 1
+        if all(translations_done):
+            break
+
+    # Iterate through each predicted example one by one;
+    # Cut-off the portion including the after the <eos> token
+    pred_sentences = []
+    for trg_sentence in trg_indexes:
+        pred_sentence = []
+        for i in range(1, len(trg_sentence)):
+            if trg_sentence[i] == trg_field.vocab.stoi[trg_field.eos_token]:
+                break
+            pred_sentence.append(trg_field.vocab.itos[trg_sentence[i]])
+        pred_sentences.append(pred_sentence)
+
+    return pred_sentences, attention
+
+def calculate_bleu_alt(iterator, src_field, trg_field, model, device, max_len = 50):
+    trgs = []
+    pred_trgs = []
+    with torch.no_grad():
+        for batch in iterator:
+            src = batch.src
+            trg = batch.trg
+            _trgs = []
+            for sentence in trg:
+                tmp = []
+                # Start from the first token which skips the <start> token
+                for i in sentence[1:]:
+                    # Targets are padded. So stop appending as soon as a padding or eos token is encountered
+                    if i == trg_field.vocab.stoi[trg_field.eos_token] or i == trg_field.vocab.stoi[trg_field.pad_token]:
+                        break
+                    tmp.append(trg_field.vocab.itos[i])
+                _trgs.append([tmp])
+            trgs += _trgs
+            pred_trg, _ = translate_sentence_vectorized(src, src_field, trg_field, model, device)
+            pred_trgs += pred_trg
+    return pred_trgs, trgs, bleu_score(pred_trgs, trgs)
+
+########################################################################
+if __name__ == "__main__":
+    run_test()
+
---
+++ transformer/transformer_implementation/inference.py
@@ -0,0 +1,149 @@
+# Resource: https://github.com/bentrevett/pytorch-seq2seq/blob/master/6%20-%20Attention%20is%20All%20You%20Need.ipynb
+from main import *
+
+"""
+Inference
+
+Now we can can translations from our model with the translate_sentence function below.
+
+The steps taken are:
+
+    tokenize the source sentence if it has not been tokenized (is a string)
+    append the <sos> and <eos> tokens
+    numericalize the source sentence
+    convert it to a tensor and add a batch dimension
+    create the source sentence mask
+    feed the source sentence and mask into the encoder
+    create a list to hold the output sentence, initialized with an <sos> token
+    while we have not hit a maximum length
+        convert the current output sentence prediction into a tensor with a batch dimension
+        create a target sentence mask
+        place the current output, encoder output and both masks into the decoder
+        get next output token prediction from decoder along with attention
+        add prediction to current output sentence prediction
+        break if the prediction was an <eos> token
+    convert the output sentence from indexes to tokens
+    return the output sentence (with the <sos> token removed) and the attention from the last layer
+"""
+def translate_sentence(sentence, src_field, trg_field, model, device, max_len = 50):
+    
+    model.eval()
+        
+    if isinstance(sentence, str):
+        nlp = spacy.load('de_core_news_sm')
+        tokens = [token.text.lower() for token in nlp(sentence)]
+    else:
+        tokens = [token.lower() for token in sentence]
+
+    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+        
+    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
+
+    src_tensor = torch.LongTensor(src_indexes).unsqueeze(0).to(device)
+    
+    src_mask = model.make_src_mask(src_tensor)
+    
+    with torch.no_grad():
+        enc_src = model.encoder(src_tensor, src_mask)
+
+    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+
+    for i in range(max_len):
+
+        trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(device)
+
+        trg_mask = model.make_trg_mask(trg_tensor)
+        
+        with torch.no_grad():
+            output, attention = model.decoder(trg_tensor, enc_src, trg_mask, src_mask)
+        
+        pred_token = output.argmax(2)[:,-1].item()
+        
+        trg_indexes.append(pred_token)
+
+        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
+            break
+    
+    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+    
+    return trg_tokens[1:], attention
+
+
+
+def display_attention(sentence, translation, attention, n_heads = 8, n_rows = 4, n_cols = 2):
+    
+    assert n_rows * n_cols == n_heads
+    
+    fig = plt.figure(figsize=(15,25))
+    
+    for i in range(n_heads):
+        
+        ax = fig.add_subplot(n_rows, n_cols, i+1)
+        
+        _attention = attention.squeeze(0)[i].cpu().detach().numpy()
+
+        cax = ax.matshow(_attention, cmap='bone')
+
+        ax.tick_params(labelsize=12)
+        ax.set_xticklabels(['']+['<sos>']+[t.lower() for t in sentence]+['<eos>'], 
+                           rotation=45)
+        ax.set_yticklabels(['']+translation)
+
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+    plt.close()
+
+def test_training_set():
+    example_idx = 8
+
+    src = vars(train_data.examples[example_idx])['src']
+    trg = vars(train_data.examples[example_idx])['trg']
+
+    print(f'src = {src}')
+    print(f'trg = {trg}')
+
+    translation, attention = translate_sentence(src, SRC, TRG, model, device)
+
+    print(f'predicted trg = {translation}')
+
+    display_attention(src, translation, attention)
+
+def test_validating_set():
+
+    example_idx = 6
+
+    src = vars(valid_data.examples[example_idx])['src']
+    trg = vars(valid_data.examples[example_idx])['trg']
+
+    print(f'src = {src}')
+    print(f'trg = {trg}')
+
+    translation, attention = translate_sentence(src, SRC, TRG, model, device)
+
+    print(f'predicted trg = {translation}')
+
+    display_attention(src, translation, attention)
+
+def test_testing_set():
+    example_idx = 10
+
+    src = vars(test_data.examples[example_idx])['src']
+    trg = vars(test_data.examples[example_idx])['trg']
+
+    print(f'src = {src}')
+    print(f'trg = {trg}')
+
+    translation, attention = translate_sentence(src, SRC, TRG, model, device)
+
+    print(f'predicted trg = {translation}')
+    display_attention(src, translation, attention)
+
+def run_test():
+    test_training_set()
+    test_validating_set()
+    test_testing_set()
+
+
+
