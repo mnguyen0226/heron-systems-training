73226594538a6bd645162024b7a36c7264107914
Minh Nguyen <mnguyen0226@vt.edu>
2021-06-01 11:12:24 -0400

    add testing script

---
+++ transformer/test_gb_transformer/test_model.py
@@ -0,0 +1,13 @@
+from main import *
+
+def test_model():
+    """Testing trained Gated Transformer
+    """
+    model.load_state_dict(torch.load('tut6-model.pt'))
+
+    test_loss = evaluate(model, test_iterator, criterion)
+
+    print(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
+
+if __name__ == "__main__":
+    test_model()
