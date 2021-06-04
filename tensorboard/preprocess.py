04403a31c3a51778e416b39173e1214823558f8b
Minh Nguyen <mnguyen0226@vt.edu>
2021-06-04 16:30:54 -0400

    add data

--- tensorboard/preprocess.py
+++ tensorboard/preprocess.py
@@ -1,26 +1,49 @@
-import tensorflow as tf
-import datetime
+# import
+import matplotlib.pyplot as plt
+import numpy as np
 
-mnist = tf.keras.datasets.mnist
+import torch
+import torchvision
+import torchvision.transforms as transforms
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0 # normalized the dataset
+# transforms for normalize the dataset from image to tensor and normalize
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))])
 
-def create_model():
-    """Create a model"""
-    return tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28,28)),
-        tf.keras.layers.Dense(512, activation = 'relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(10, activateion = 'softmax')
-    ])
+# datasets
+trainset = torchvision.datasets.FashionMNIST('./data', 
+    download=True,
+    train=True,
+    transform=transform)
+testset = torchvision.datasets.FashionMNIST('./data', 
+    download=True,
+    train=False,
+    transform=transform)
 
-def model_fit():
-    model = create_model()
-    model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy', metrics=['accuracy'])
+# dataloaders
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
-    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+# constant for classes
+classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+        'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
 
+# helper function to show an image
+def matplotlib_imshow(img, one_channel=False): # show 1 color channel or not
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5 # unnormalize
+    npimg = img.numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1,2,0)))
+
+def main():
+    print("Running preprocess")
 
 if __name__ == "__main__":
-    model_fit()
+    if __name__ == '__main__':
+        main()
+        
