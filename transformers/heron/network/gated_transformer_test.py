0722a1ec821fad62f29fc83760f84eb321547554
Minh Nguyen <mnguyen0226@vt.edu>
2021-06-07 16:54:04 -0400

    heron

---
+++ transformers/heron/network/gated_transformer_test.py
@@ -0,0 +1,169 @@
+from collections import OrderedDict
+from typing import Tuple
+
+import numpy as np
+import pytest
+import torch
+from torch import nn
+
+from gamebreaker.classifier.network.gated_decoder import Decoder
+from gamebreaker.classifier.network.gated_encoder import Encoder
+
+
+class AutoRegTransformer(nn.Module):
+    def __init__(
+        self,
+        input_shape: Tuple[int, int],
+        nb_heads: int,
+        max_seq_len: int,
+        scale: bool,
+        dropout: float,
+    ):
+        super().__init__()
+        self.max_seq_len = max_seq_len + 1
+        self.encoder = Encoder(input_shape, nb_heads, scale, dropout)
+        self.decoder = Decoder(input_shape, max_seq_len + 1, nb_heads, scale, dropout)
+        self.output_layers = nn.Sequential(
+            OrderedDict(
+                [
+                    ("flatten", nn.Flatten()),
+                    (
+                        "linear_out",
+                        nn.Linear(
+                            input_shape[0] * (max_seq_len + 1),
+                            input_shape[-1] + 1,
+                            bias=False,
+                        ),
+                    ),
+                    # ("softmax_out", nn.Softmax(dim=-1)),
+                ]
+            )
+        )
+
+    def forward(self, xs: torch.Tensor) -> torch.Tensor:
+        b, f, s = xs.shape
+        decoder_choices = torch.zeros((b, f, s + 1)).to(xs.device)
+        decoder_choices[:, :, :-1] = torch.clone(xs)
+        decoder_choices[:, :, -1] = -1
+        encoded_xs = self.encoder(xs)
+        outputs = torch.zeros(b, self.max_seq_len, s + 1).to(xs.device)
+        prev_seq = torch.zeros(b, f, self.max_seq_len).to(xs.device)
+        for t in range(self.max_seq_len):
+            decoder_out = self.decoder(prev_seq, encoded_xs)
+            new_outputs = self.output_layers(decoder_out)
+            selected_indices = torch.argmax(new_outputs, dim=-1)
+            prev_seq[list(range(b)), :, t] = decoder_choices[list(range(b)), :, selected_indices]
+            outputs[:, t, :] = new_outputs
+
+        return outputs
+
+
+def binary(x: torch.Tensor, bits: int) -> torch.Tensor:
+    """Converts x to binary numbers of len(bits)
+
+    Parameters
+    ----------
+    x: torch.Tensor
+        Numbers to convert to binary
+    bits: int
+        Bit length of output
+
+    Returns
+    -------
+    torch.Tensor
+        Binary representations of x
+    """
+    mask = 2 ** torch.arange(bits).to(x.device, x.dtype)
+    return x.unsqueeze(-1).bitwise_and(mask).ne(0).byte()
+
+
+def test_autoreg():
+    nb_bits = 4
+    sequence_len = 8
+    nb_samples = 9600
+    train_samples = nb_samples - 1600
+    batch_size = 64
+    nb_epochs = 100
+
+    transformer = AutoRegTransformer((nb_bits, sequence_len), 1, sequence_len, False, 0.0).to(0)
+    optim = torch.optim.Adam(transformer.parameters(), 0.001)
+    loss_fcn = nn.CrossEntropyLoss()
+    possible_labels = list(range(1, 2 ** nb_bits))
+
+    permutations = torch.tensor(
+        [np.random.choice(possible_labels, sequence_len, replace=False) for _ in range(nb_samples)]
+    )
+    permutations = torch.cat(
+        (permutations, (2 ** nb_bits) * torch.ones(permutations.shape[0], 1)), dim=1
+    ).long()
+    order, labels = torch.sort(permutations)
+
+    labels = torch.where(order <= (2 ** (nb_bits - 1)), labels, -1 * torch.ones_like(labels))
+    labels = torch.where(
+        order == (2 ** (nb_bits - 1)), sequence_len * torch.ones_like(labels), labels
+    )
+
+    inputs = binary(permutations[:, :-1], nb_bits).float()
+
+    training_inputs = inputs[0:train_samples]
+    training_labels = labels[0:train_samples]
+
+    x_train = torch.reshape(training_inputs, (-1, batch_size, sequence_len, nb_bits)).permute(
+        0, 1, 3, 2
+    )
+    y_train = torch.reshape(training_labels, (-1, batch_size, sequence_len + 1))
+
+    validation_inputs = inputs[train_samples:]
+    validation_labels = labels[train_samples:]
+
+    x_val = torch.reshape(validation_inputs, (-1, batch_size, sequence_len, nb_bits)).permute(
+        0, 1, 3, 2
+    )
+    y_val = torch.reshape(validation_labels, (-1, batch_size, sequence_len + 1))
+
+    max_val_acc = 0
+    for epoch in range(nb_epochs):
+        train_acc = torch.zeros(1).to(0)
+        train_denom = 0
+        for x_batch, y_batch in zip(x_train, y_train):
+            optim.zero_grad()
+            y_pred = transformer(x_batch.to(0))
+            batch_mask = y_batch.view(-1) > 0
+            loss = loss_fcn(
+                y_pred.view(-1, sequence_len + 1)[batch_mask, :],
+                y_batch.view(-1)[batch_mask].to(0).long(),
+            )
+            loss.backward()
+            optim.step()
+            train_acc += torch.sum(
+                torch.argmax(y_pred.view(-1, sequence_len + 1)[batch_mask, :], dim=-1)
+                == y_batch.view(-1)[batch_mask].to(0)
+            )
+            train_denom += len(y_batch.view(-1)[batch_mask])
+
+        with torch.no_grad():
+            avg_acc = torch.zeros(1).to(0)
+            denom = 0
+            for x_batch, y_batch in zip(x_val, y_val):
+                y_pred = transformer(x_batch.to(0))
+                batch_mask = y_batch.view(-1) > 0
+                avg_acc += torch.sum(
+                    torch.argmax(y_pred.view(-1, sequence_len + 1)[batch_mask, :], dim=-1)
+                    == y_batch.view(-1)[batch_mask].to(0)
+                )
+                denom += len(y_batch.view(-1)[batch_mask])
+            avg_acc = avg_acc / denom
+            train_acc = train_acc / train_denom
+            print(
+                f"[{epoch:3d}] Training Acc: {train_acc.item(): 0.3f}\tValidation Acc: {avg_acc.item(): 0.3f}"
+            )
+            if avg_acc.item() > max_val_acc:
+                max_val_acc = avg_acc.item()
+                if max_val_acc > 0.99:
+                    break
+
+    assert max_val_acc > 0.99
+
+
+torch.autograd.set_detect_anomaly(True)
+test_autoreg()
---
+++ transformers/heron/network/gb_transformers_test/nlp_transformers_test.py
@@ -0,0 +1,276 @@
+# pytest script that make sure the final train/validate loss and train/validate PPL of the gated transformers is
+# better than the original transformers from "Attention Is All You Need"
+# We want the Train Loss, Val Loss, Train PPL, and Val PPL to be lower than the test bench
+# RUNNING COMMAND: pytest -s pytest_transformers.py
+import pytest
+from decimal import Decimal
+import torch
+import torch.nn as nn
+from utils.preprocess import run_preprocess, device
+
+from utils.original_transformers.training_utils import (
+    origin_transformers_main,
+    test_origin_transformers_model,
+)
+from utils.original_transformers.encoder import Encoder
+from utils.original_transformers.decoder import Decoder
+from utils.original_transformers.seq2seq import Seq2Seq
+from utils.original_transformers.training_utils import (
+    initialize_weights,
+    origin_transformers_main,
+    test_origin_transformers_model,
+)
+
+from utils.gated_transformers.training_utils import (
+    gated_transformers_main,
+    test_gated_transformers_model,
+)
+from utils.gated_transformers.encoder import Encoder
+from utils.gated_transformers.decoder import Decoder
+from utils.gated_transformers.seq2seq import Seq2Seq
+from utils.gated_transformers.training_utils import (
+    initialize_weights,
+    gated_transformers_main,
+    test_gated_transformers_model,
+)
+
+
+def gated_model_train():
+    """Creates a training function for Gated Transformers"""
+    train_iterator, valid_iterator, test_iterator, SRC, TRG = run_preprocess()
+
+    INPUT_DIM = len(SRC.vocab)
+    OUTPUT_DIM = len(TRG.vocab)
+    HID_DIM = 256
+    GATED_ENC_LAYERS = 3
+    GATED_DEC_LAYERS = 3
+    GATED_ENC_HEADS = 8
+    GATED_DEC_HEADS = 8
+    ENC_PF_DIM = 512
+    DEC_PF_DIM = 512
+    ENC_DROPOUT = 0.1
+    DEC_DROPOUT = 0.1
+
+    # Initializes variables for training process
+    N_EPOCHS = 10
+    CLIP = 1
+    LEARNING_RATE = 0.0005
+
+    enc = Encoder(
+        INPUT_DIM, HID_DIM, GATED_ENC_LAYERS, GATED_ENC_HEADS, ENC_PF_DIM, ENC_DROPOUT, device
+    )
+    dec = Decoder(
+        OUTPUT_DIM, HID_DIM, GATED_DEC_LAYERS, GATED_DEC_HEADS, DEC_PF_DIM, DEC_DROPOUT, device
+    )
+
+    # Define whole Seq2Seq encapsulating model
+    SRC_PAD_IDX = SRC.vocab.stoi[SRC.pad_token]
+    TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
+
+    model = Seq2Seq(enc, dec, SRC_PAD_IDX, TRG_PAD_IDX, device).to(device)
+
+    # Initialize the model's weights
+    model.apply(initialize_weights)
+
+    # Adam optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    # Cross Entropy Loss Function
+    criterion = nn.CrossEntropyLoss(ignore_index=TRG_PAD_IDX)
+
+    gated_transformers_enc_layers = GATED_ENC_LAYERS
+    gated_transformers_dec_layers = GATED_DEC_LAYERS
+    gated_transformers_enc_n_heads = GATED_ENC_HEADS
+    gated_transformers_dec_n_heads = GATED_DEC_HEADS
+
+    # results from Gated Transformers
+    print(
+        "\n------------------------------------------------------------------------------------------"
+    )
+    print(
+        f"\nThe gated Transformer has {gated_transformers_enc_n_heads} encoder head(s), {gated_transformers_dec_n_heads} \
+    decoder head(s), {gated_transformers_enc_layers} encoder layer(s), {gated_transformers_dec_layers} decoder layer(s)"
+    )
+
+    print("-----------------------------------------")
+    print("Traininng Gated Transformers Training Set")
+    print("-----------------------------------------")
+    (
+        gated_transformers_training_loss,
+        gated_transformers_validating_loss,
+        gated_transformers_training_PPL,
+        gated_transformers_validating_PPL,
+    ) = gated_transformers_main(
+        model=model,
+        train_iterator=train_iterator,
+        optimizer=optimizer,
+        criterion=criterion,
+        CLIP=CLIP,
+        valid_iterator=valid_iterator,
+        n_epochs=N_EPOCHS,
+    )
+
+    print("----------------------------------------")
+    print("Traininng Gated Transformers Testing Set")
+    print("----------------------------------------")
+    (
+        gated_transformers_testing_loss,
+        gated_transformers_testing_PPL,
+    ) = test_gated_transformers_model(model=model, test_iterator=test_iterator, criterion=criterion)
+
+    return (
+        gated_transformers_training_loss,
+        gated_transformers_validating_loss,
+        gated_transformers_training_PPL,
+        gated_transformers_validating_PPL,
+        gated_transformers_testing_loss,
+        gated_transformers_testing_PPL,
+    )
+
+
+def original_model_train():
+    """Creates a training function for Original Transformers"""
+    train_iterator, valid_iterator, test_iterator, SRC, TRG = run_preprocess()
+
+    INPUT_DIM = len(SRC.vocab)
+    OUTPUT_DIM = len(TRG.vocab)
+    HID_DIM = 256
+    ENC_LAYERS = 3
+    DEC_LAYERS = 3
+    ENC_HEADS = 8
+    DEC_HEADS = 8
+    ENC_PF_DIM = 512
+    DEC_PF_DIM = 512
+    ENC_DROPOUT = 0.1
+    DEC_DROPOUT = 0.1
+
+    # Initializes variables for training process
+    N_EPOCHS = 10
+    CLIP = 1
+    LEARNING_RATE = 0.0005
+
+    enc = Encoder(INPUT_DIM, HID_DIM, ENC_LAYERS, ENC_HEADS, ENC_PF_DIM, ENC_DROPOUT, device)
+    dec = Decoder(OUTPUT_DIM, HID_DIM, DEC_LAYERS, DEC_HEADS, DEC_PF_DIM, DEC_DROPOUT, device)
+
+    # Define whole Seq2Seq encapsulating model
+    SRC_PAD_IDX = SRC.vocab.stoi[SRC.pad_token]
+    TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
+
+    model = Seq2Seq(enc, dec, SRC_PAD_IDX, TRG_PAD_IDX, device).to(device)
+
+    # Initialize the model's weights
+    model.apply(initialize_weights)
+
+    # Adam optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    # Cross Entropy Loss Function
+    criterion = nn.CrossEntropyLoss(ignore_index=TRG_PAD_IDX)
+
+    origin_transformers_enc_layers = ENC_LAYERS
+    origin_transformers_dec_layers = DEC_LAYERS
+    origin_transformers_enc_n_heads = ENC_HEADS
+    origin_transformers_dec_n_heads = DEC_HEADS
+
+    # results from Transformers "Attention Is All You Need"
+    print(
+        f"\nThe original Transformer has {origin_transformers_enc_n_heads} encoder head(s), {origin_transformers_dec_n_heads} \
+    decoder head(s), {origin_transformers_enc_layers} encoder layer(s), {origin_transformers_dec_layers} decoder layer(s)"
+    )
+
+    print("---------------------------------------------")
+    print("Training Test Bench Transformers Training Set")
+    print("---------------------------------------------")
+    (
+        origin_transformers_training_loss,
+        origin_transformers_validating_loss,
+        origin_transformers_training_PPL,
+        origin_transformers_validating_PPL,
+    ) = origin_transformers_main(
+        model=model,
+        train_iterator=train_iterator,
+        optimizer=optimizer,
+        criterion=criterion,
+        CLIP=CLIP,
+        valid_iterator=valid_iterator,
+        n_epochs=N_EPOCHS,
+    )
+
+    print("\n--------------------------------------------")
+    print("Training Test Bench Transformers Testing Set")
+    print("--------------------------------------------")
+    (
+        origin_transformers_testing_loss,
+        origin_transformers_testing_PPL,
+    ) = test_origin_transformers_model(
+        model=model, test_iterator=test_iterator, criterion=criterion
+    )
+
+    return (
+        origin_transformers_training_loss,
+        origin_transformers_validating_loss,
+        origin_transformers_training_PPL,
+        origin_transformers_validating_PPL,
+        origin_transformers_testing_loss,
+        origin_transformers_testing_PPL,
+    )
+
+
+# Run training function
+(
+    gated_transformers_training_loss,
+    gated_transformers_validating_loss,
+    gated_transformers_training_PPL,
+    gated_transformers_validating_PPL,
+    gated_transformers_testing_loss,
+    gated_transformers_testing_PPL,
+) = gated_model_train()
+
+(
+    origin_transformers_training_loss,
+    origin_transformers_validating_loss,
+    origin_transformers_training_PPL,
+    origin_transformers_validating_PPL,
+    origin_transformers_testing_loss,
+    origin_transformers_testing_PPL,
+) = original_model_train()
+
+
+class TestGatedTransformersTrainingSet:
+    def test_training_loss(self):
+        """Validated the training loss of gated transformers < original transformers'"""
+        global gated_transformers_training_loss, origin_transformers_training_loss
+        assert Decimal(gated_transformers_training_loss) < Decimal(
+            origin_transformers_training_loss
+        )
+
+    def test_validating_loss(self):
+        """Validates the validating loss of gated transformers < original transformers'"""
+        global gated_transformers_validating_loss, origin_transformers_validating_loss
+        assert Decimal(gated_transformers_validating_loss) < Decimal(
+            origin_transformers_validating_loss
+        )
+
+    def test_training_PPL(self):
+        """Validates the training PPL of gated transformers < original transformers'"""
+        global gated_transformers_training_PPL, origin_transformers_training_PPL
+        assert Decimal(gated_transformers_training_PPL) < Decimal(origin_transformers_training_PPL)
+
+    def test_validating_PPL(self):
+        """Validates the validating PPL of gated transfomers < original transformers'"""
+        global gated_transformers_validating_PPL, origin_transformers_validating_PPL
+        assert Decimal(gated_transformers_validating_PPL) < Decimal(
+            origin_transformers_validating_PPL
+        )
+
+
+class TestGatedTransformersTestingSet:
+    def test_testing_loss(self):
+        """Validates the testing loss of the gated transformers < origin transformers'"""
+        global gated_transformers_testing_loss, origin_transformers_testing_loss
+        assert Decimal(gated_transformers_testing_loss) < Decimal(origin_transformers_testing_loss)
+
+    def test_testing_PPL(self):
+        """Validates the testing PPL of the gated transformers < origin transformers'"""
+        global gated_transformers_testing_PPL, origin_transformers_testing_PPL
+        assert Decimal(gated_transformers_testing_PPL) < Decimal(origin_transformers_testing_PPL)
